11e384b7cf61f3468adf4dcb0cc1a6a28f361249
brewchetta <chett.tiller@gmail.com>
2020-04-17 09:48:44 -0400

    quick enemy builder form

--- src/views/EnemyBuilderMain.vue
+++ src/views/EnemyBuilderMain.vue
@@ -5,12 +5,70 @@
   <!-- in future versions this should auto-add a CR -->
 
 <template lang="html">
-  <h1>Enemy Builder (Main View)</h1>
+  <div class="enemy-builder-main">
+
+    <h1>Enemy Builder (Main View)</h1>
+
+    <form>
+
+      <label>Name</label>
+      <input v-model="name"
+      type="text"
+      name="name"
+      value="">
+
+      <br/>
+
+      <label>Alignment (switch to two radio buttons?)</label>
+      <input v-model="alignment"
+      type="text"
+      name="alignment"
+      value="">
+
+      <br/>
+
+      <label>Challenge Rating (select dropdown in order to get the 1/8 and stuff)</label>
+      <input v-model="challengeRating"
+      type="text"
+      name="challenge-rating"
+      value="">
+
+      <br/>
+
+      <label>Armor Class (number)</label>
+      <input v-model="armorClass"
+      type="number"
+      name="armor-class"
+      value="">
+
+      <br/>
+
+      <label>Hit Points (number)</label>
+      <input v-model="hitPoints"
+      type="number"
+      name="hit-points"
+      value="">
+
+    </form>
+
+    <p v-show="name.length">Name: {{name}}</p>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'EnemyBuilderMain'
+  name: 'EnemyBuilderMain',
+  data() {
+    return {
+      name: "",
+      alignment: "",
+      challengeRating: "0",
+      armorClass: 10,
+      hitPoints: 10
+      // abilities ought to be a seperate set of components created by an array here
+    }
+  }
 }
 </script>
 
